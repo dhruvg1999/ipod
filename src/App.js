1ee66aa03cb3ad898a73980356c005d367dbbd04
Dhruv Goyal <dhruv1636.cse19@chitkara.edu.in>
2021-11-03 11:45:45 +0530

    1st Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import IpodBody from './IpodBody';
+
+function App() {
+  return (
+    <div className="App">
+      <IpodBody/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/IpodBody.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class IpodBody extends React.Component{
+    render(){
+        return(
+            <div className="ipod">
+                <div className="screen">
+                </div>
+                <div className="scroller"></div>
+                <div className="ok-button"></div>
+            </div>
+        );
+    }
+}
+
+export default IpodBody;
